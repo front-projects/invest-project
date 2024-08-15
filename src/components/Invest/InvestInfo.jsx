ae1fec4742816b0bae3e8d0f2ba52738fcb86673
Front-projects <sasnykvlad2@gmail.com>
2024-08-15 17:32:41 +0300

    upd

--- src/components/Invest/InvestInfo.jsx
+++ src/components/Invest/InvestInfo.jsx
@@ -5,15 +5,15 @@ export default function InvestInfo() {
     <div className="invest-info text-center text-[15px] invest-gradient px-[10%] max-small:text-[13px] max-small:h-[78px] h-[90px] min-h-[90px]">
       <ReactTypingEffect
         eraseDelay={999999}
-        speed={50}
+        speed={20}
         typingDelay={200}
         text="Invest cryptocurrencies to speed up the work of blockchain chains and get from 0.8 to 3.00% of net profit daily"
       />
 
       <ReactTypingEffect
         eraseDelay={999999}
-        speed={50}
-        typingDelay={6400}
+        speed={20}
+        typingDelay={3000}
         text="To activate the bot, top up your balance"
       />
     </div>
