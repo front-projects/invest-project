0f920c71e83de35eb41333fe01bd92d2b0cf8482
Front-projects <sasnykvlad2@gmail.com>
2024-08-15 16:36:00 +0300

    upd

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,7 @@ export default {
       lg: "400px",
       xl: "460px",
       "max-small": { raw: "(max-height: 706px)" },
+      "max-xsmall": { raw: "(max-height: 640px)" },
     },
     extend: {},
   },
